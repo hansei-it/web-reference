c49042ceb73b43f3539b46b5bd41c738533f86e8
DongHwan Gong <armyvision@hansei.ac.kr>
2022-09-27 12:35:33 +0900

    express.static() 미들웨어 사용한 정적 서버
    파일을 발견하면 전송하고  next()를 호출하지 않고 없으면 다음 미들웨어 실행을 위해 next()를 호출.

--- back-end/app.js
+++ back-end/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require('path');
 
 const app = express();
-app.listen(7070, () => {
-  console.log(7070, "번 포트에서 대기 중");
+app.set('port', process.env.PORT || 7070);
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), "번 포트에서 대기 중");
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/pub/index.html"));
-});
+// '/'로 접속하면 pub 디렉토리를 정적파일 위치로 지정
+app.use('/', express.static(path.join(__dirname, 'pub')));
