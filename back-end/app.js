c2c8d11d8452c3e257f39e5322e01130e88f364f
DongHwan Gong <armyvision@hansei.ac.kr>
2022-09-27 13:23:18 +0900

    콘솔로그 출력을 위한 morgan 모듈 설치, 환경파일  dotenv 모듈 설치
    모든 접속에  body, query, params 정보 출력을 위한 미들웨어 설치

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require('path');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config();
 
 const app = express();
 app.set('port', process.env.PORT || 7070);
@@ -8,5 +12,19 @@ app.listen(app.get('port'), () => {
   console.log(app.get('port'), "번 포트에서 대기 중");
 });
 
+//모든 접속의 필요 정보 출력
+app.use('*', (req,res,next)=>{
+    console.log('++++++++++++++++++++++++++++++++++');
+    console.log('=========== req.body =============');
+    console.log(req.body)
+    console.log('=========== req.query =============');
+    console.log(req.query)
+    console.log('=========== req.params =============');
+    console.log(req.params)
+    console.log('=========== process.env.RUN_TYPE =============');
+    console.log(process.env.RUN_TYPE)
+    next();
+})
+app.use(morgan('dev'));//dev,combined,common,short,tiny등
 // '/'로 접속하면 pub 디렉토리를 정적파일 위치로 지정
 app.use('/', express.static(path.join(__dirname, 'pub')));
